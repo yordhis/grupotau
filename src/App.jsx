47e019e67c8a634e47c1fea1263a0e376f8dd948
yordhis <yordhis.10@gmail.com>
2023-10-05 10:35:19 -0400

    grupo tau en construccion

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,22 @@ function App() {
   const mensaje = "Saludos me gustaria conocer los servicio que ofrece su empresa."
   return (
     <>
+    
       <div>
         <a href="#" target="_self">
-          <img src={logo} className="logo" alt="Logo InverTau" />
+          <img src={logo} className="logo" alt="Logo GrupoTau" />
         </a>
        
       </div> 
       <h1>Grupotau.net</h1>
       <h4>Próximamente - Sitio en construcción</h4>
       <div className="card">
-        <a href="https://wa.me/581811469?text=${mensaje}" className='icono-whatsapp'>
+        <a href="https://wa.me/581811469?text='Saludos, Quiero conocer sus servicios.'" className='icono-whatsapp'>
           <FaWhatsapp />
         </a>
+        <p className='text-info'>
+          Escríbenos por whatsapp y conoce nuestro servicios
+        </p>
 
         <p className='footer'>
           Desarrollado por <a href="https://cyberstaffstore.com">
